5966e4ebcd2303c2750507d1278fb0924c96ee63
肖镇龙 <xzlxiao@outlook.com>
2015-09-17 17:03:17 +0800

    Initial Commit

---
+++ 图书管理系统/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  图书管理系统
+//
+//  Created by 肖镇龙 on 15/9/17.
+//  Copyright (c) 2015年 肖镇龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
